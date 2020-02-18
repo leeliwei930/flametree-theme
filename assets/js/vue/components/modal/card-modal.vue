a02cf57b7d16e034c7c19e435c2d9277d8842b2d
Lee Li Wei <leeliwei930@gmail.com>
2020-02-18 17:22:38 +0800

    Update the Membership authentication UI layout
    - Add WIP card-modal vue components

---
+++ assets/js/vue/components/modal/card-modal.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    .modal.fade(role="dialog" tabindex="-1")
+        .modal-dialog
+            .modal-content
+                h5.modal-title {{ title }}
+
+</template>
+<style lang="scss">
+
+</style>
+<script type="text/javascript">
+export default {
+    props: {
+
+    },
+    data: function(){
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    created: function(){
+
+    },
+    mounted: function(){
+
+    },
+    updated: function(){
+
+    }
+}
+
+</script>
