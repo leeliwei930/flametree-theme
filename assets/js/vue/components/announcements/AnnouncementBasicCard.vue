bd3247b467ddaaf7e1686ce34cf64c5357a62cc0
Lee Li Wei <leeliwei930@gmail.com>
2020-05-30 14:13:34 +1000

    Update the announcement-basic-card vue component slot placing ordering

--- assets/js/vue/components/announcements/AnnouncementBasicCard.vue
+++ assets/js/vue/components/announcements/AnnouncementBasicCard.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
-    .announcement-basic-card.d-flex.flex-column.shadow.bg-light.p-3(:class="{'hoverable' : hoverable}")
-        h2.post-title.text-dark {{title}}
-        p.post-summary.text-dark {{ description }}
-        p
-            i.fas.fa-user.text-accent.mr-2
-            | {{ author }}
-
-        .d-flex.flex-column.justify-content-start.flex-wrap
+    .announcement-basic-card.d-flex.flex-column.shadow.bg-light.p-3.justify-content-between(:class="{'hoverable' : hoverable}")
+        .d-flex.flex-column
+            h2.post-title.text-dark {{title}}
+            p.post-summary.text-dark {{ description }}
             p
-                i.fas.fa-calendar.text-accent.mr-2
-                | {{ publishedOn }}
-            slot
+                i.fas.fa-user.text-accent.mr-2
+                | {{ author }}
+
+            .d-flex.flex-column.justify-content-start.flex-wrap
+                p
+                    i.fas.fa-calendar.text-accent.mr-2
+                    | {{ publishedOn }}
+        slot
 </template>
 <style lang="scss">
     .announcement-basic-card {
@@ -18,7 +19,7 @@
 
         padding:15px;
         width: 480px;
-        height: 100%;
+
         transition: all 0.25s;
         &.hoverable:hover {
             box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.5) !important;
